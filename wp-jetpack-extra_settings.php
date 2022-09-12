1923820df2d46c33c6c0fb403baf240be7d0d55b
Daniel Vena <deaconvena+github@gmail.com>
2022-09-12 15:30:49 -0400

    formatting and version bump

--- wp-jetpack-extra_settings.php
+++ wp-jetpack-extra_settings.php
@@ -4,13 +4,17 @@
 * Plugin URI: github.com/cumulus-digital/wp-jetpack-extra_settings
 * GitHub Plugin URI: cumulus-digital/wp-jetpack-extra_settings
 * Description: A simple plugin to add additional settings to Jetpack
-* Version:  0.1
+* Version:  0.2
 * Author: Daniel Vena
 * Author URI: cumulusmedia.com
 * License: GPL2
 */
+
 namespace WP_JETPACK_EXTRA;
-if (!defined('ABSPATH')) die('No direct access allowed');
+
+if (!defined('ABSPATH')) {
+    die('No direct access allowed');
+}
 
 const PLUGIN_NAME = 'wp-jetpack-extra_settings';
 const PREFIX = 'wp-jetpack-extra_settings';
